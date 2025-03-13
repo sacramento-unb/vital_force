c99042c17c379c717c44a18d9e57316e0591d8c7
sacramento-unb <160270409+sacramento-unb@users.noreply.github.com>
2025-03-13 09:13:52 -0300

    Update home.py

--- home.py
+++ home.py
@@ -4,11 +4,11 @@ import folium
 from streamlit_folium import st_folium
 
 def wide_space_default():
-st.set_page_config(layout="wide")
-
-wide_space_default()
-
-st.sidebar.write('Menu')
+    st.set_page_config(layout="wide")
+    
+    wide_space_default()
+    
+    st.sidebar.write('Menu')
 
 # Load data
 @st.cache_resource
